e34d06f530e883101a516779dce08411b25056a1
asyqsg <43290140+asyqsg@users.noreply.github.com>
2020-11-12 09:15:42 +0800

    202011/12‘

--- 排序.py
+++ 排序.py
@@ -1,84 +1,49 @@
-'''
-acwing 快排
-'''
-import sys
-data = sys.stdin.readline().strip()
-data = data.split()
-data = list(map(int,data))
+def quick_sort(nums,l,r):
+    if l >= r: return
+    
+    flag_num = nums[l]
+    i = l-1
+    j = r+1
+    while i < j:
+        while True:
+            i+=1
+            if nums[i] >= flag_num:
+                break
+        while True:
+            j-=1
+            if nums[j] <= flag_num:
+                break
+        
+        if i < j:
+            nums[i],nums[j] = nums[j],nums[i]
+    quick_sort(nums,l,j)
+    quick_sort(nums,j+1,r)
+    return nums
 
-class Sort:
-    def quick_sort_more(self,nums:list):
-        '''
-        快排需要额外的内存空间   不推荐但是好理解
-        :param nums: 输入的list
-        :return:   返回排序完成的list
-        '''
-        if len(nums) < 2:
-            return nums
-
-        mid = nums[len(nums)//2]
-        left,right = [],[]
-        nums.remove(mid)
-        for item in nums:
-            if item >= mid:
-                right.append(item)
-            else:
-                left.append(item)
-        return self.quick_sort_more(left) + [mid] + self.quick_sort_more(right)
-
-    def quick_sort_recommand(self,nums:list,l_index,r_index):
-        #根据acwing推荐算法
-        if l_index >= r_index: return   #注意！！！！！
-
-        flag_num = nums[l_index]
-        i = l_index-1
-        j = r_index+1
-        while i < j:
-            while True:
-                i+=1
-                if nums[i] >= flag_num:
-                    break
-            while True:
-                j-=1
-                if nums[j] <= flag_num:
-                    break
-            if i < j:
-                nums[i],nums[j] = nums[j],nums[i]
-        # nums[l] = flag_num
-        self.quick_sort_recommand(nums,l_index,j)
-        self.quick_sort_recommand(nums,j+1,r_index)
-        return nums
-
-    def qiuck_sort(self,nums,l,r):
-        if l >= r: return
-
-        flag_num = nums[0]
-        i,j = l-1,r+1
-        while i < j:
-            while True:
-                i+=1
-                if nums[i] >= flag_num:
-                    break
-
-            while True:
-                j-=1
-                if nums[j] <= flag_num:
-                    break
-            if i < j:
-                nums[i],nums[j] = nums[j],nums[i]
-        self.qiuck_sort(nums,l,j)
-        self.quick_sort(nums,j+1,r)
-        return nums
-
-
-
-
-if __name__ == '__main__':
-    size = len(data)
-    l,r = 0,len(data)-1
-    print(Sort().quick_sort_recommand(data,l,r))
-    print(Sort().quick_sort_more(data))
-    # # print(Sort().merge_sort(data))
-    # Sort().merge_sort(data)
-    # print(data)
-    #
+def merge_sort(nums):
+    size = len(nums)
+    mid = size//2
+    L = nums[:mid]
+    R = nums[mid:]
+    merge_sort(L)
+    merge_sort(R)
+    
+    i,j,k =0,0,0
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            nums[k] = L[i]
+            i+=1
+        else:
+            nums[k] = R[j]
+            j+=1
+        k+=1
+    while i < len(L):
+        nums[k] = L[i]
+        i+=1
+        k+=1
+    while j < len(R):
+        nums[k] = R[j]
+        j+=1
+        k+=1
+    
+    return nums
